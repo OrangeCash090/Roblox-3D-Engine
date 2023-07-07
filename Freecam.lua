b0fd11e15b8d493bd9755ea7f51ffc5e32d5529d
OrangeCash090 <97858392+OrangeCash090@users.noreply.github.com>
2023-07-07 14:31:31 -0400

    Create Freecam.lua

---
+++ Freecam.lua
@@ -0,0 +1,19 @@
+-- Modules --
+local Engine3D = require(Modules.Engine3D)
+local ModelLoader = require(Modules.ModelLoader)
+local Collision = require(Modules.Collision)
+local CameraModule = require(Modules.Camera)
+local WeldModule = require(Modules.Weld)
+local AnimationModule = require(Modules.Animation)
+local PlayerModule = require(Modules.Player)
+
+local Mouse = game.Players.LocalPlayer:GetMouse()
+local Camera = CameraModule.new(Vector3.new(0,5,0), Vector3.zero, 1/50, "Freecam")
+local World = Engine3D.new(script.Parent.Screen, Vector2.new(128,128), Camera, 70, false)
+
+local Cube = ModelLoader.LoadFromName("Cube")
+
+game:GetService("RunService").RenderStepped:Connect(function(dt)
+	Camera:Update(true)
+	World:Update()
+end)
